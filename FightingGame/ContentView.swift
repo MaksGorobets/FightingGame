a4a3928c23615c976a4dbc7a1d6e281fa0c9175a
MaksGorobets <gorobets542@gmail.com>
2023-09-16 18:50:38 +0300

    Added character images (will change later), winner bottomSheet, fighting action

--- FightingGame/ContentView.swift
+++ FightingGame/ContentView.swift
@@ -9,14 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var character1: Character?
+    @State var character1: Character?
+    @State var character2: Character?
+    @State var isButtonPressed: Bool = false
+    @State var showSheet: Bool = false
+    @State var buttonDisabled: Bool = false
+    @State var winner: String = ""
+    @State var winnerImage: String = ""
+    let character1Image = Int.random(in: 0...5)
+    let character2Image = Int.random(in: 0...5)
+    let charactersArt = ["character0", "character1", "character2", "character3", "character4", "character5"]
+    
+    
+    private func fetchCharacters() {
+            Task {
+                do {
+                    character1 = try await getCharacter1()
+                } catch GHError.invalidURL {
+                    print("Invalid URL")
+                } catch GHError.invalidResponse {
+                    print("Invalid response")
+                } catch GHError.invalidData {
+                    print("Invalid data")
+                } catch {
+                    print("Unexpected error")
+                }
+                
+                do {
+                    character2 = try await getCharacter2()
+                } catch GHError.invalidURL {
+                    print("Invalid URL")
+                } catch GHError.invalidResponse {
+                    print("Invalid response")
+                } catch GHError.invalidData {
+                    print("Invalid data")
+                } catch {
+                    print("Unexpected error")
+                }
+                
+                // Reset the button press flag after fetching characters
+                isButtonPressed = false
+            }
+        }
+    
+    func checkIfAlive() {
+        if character1?.alive == false {
+            showSheet = true
+            buttonDisabled = true
+            winner = "\(character2?.name ?? "Unknown")"
+            winnerImage = charactersArt[character2Image]
+        } else if character2?.alive == false {
+            showSheet = true
+            buttonDisabled = true
+            winner = "\(character1?.name ?? "Unknown")"
+            winnerImage = charactersArt[character1Image]
+        }
+    }
     
     var body: some View {
         NavigationView{
             VStack {
                 HStack{
-                    Image(systemName: "person.circle")
+                    Image(charactersArt[character1Image])
                         .resizable()
+                        .clipShape(Circle())
                         .frame(width: 70, height: 70)
                         .padding()
                     Text(character1?.name ?? "No name")
@@ -38,52 +94,55 @@ struct ContentView: View {
                 .font(.system(size: 20))
                 .padding()
                 HStack{
-                    Image(systemName: "person.circle")
+                    Image(charactersArt[character2Image])
                         .resizable()
+                        .clipShape(Circle())
                         .frame(width: 70, height: 70)
                         .padding()
-                    Text("Name")
+                    Text(character2?.name ?? "No name")
                         .font(.system(size: 30))
                     Spacer()
                 }
                 VStack{
-                    Text("Level: 12, XP: 80")
+                    Text("Level: \(character2?.level ?? 0), XP: \(character2?.xp ?? 0)")
                         .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("Health 110: , Armor: 15")
+                    Text("Health \(character2?.health ?? 0): , Armor: \(character2?.armor ?? 0)")
                         .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("Attack: 6, CrircalAttack: 12")
+                    Text("Attack: \(character2?.attack ?? 0), CrircalAttack: \(character2?.criticalAttack ?? 0)")
                         .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("Luck: 5")
+                    Text("Luck: \(character2?.luck ?? 0)")
                         .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("Balance: 200$")
+                    Text("Balance: \(character2?.balance ?? 0)$")
                         .frame(maxWidth: .infinity, alignment: .leading)
                 }
                 .font(.system(size: 20))
                 .padding()
                 Spacer()
                 Button {
-                    
+                    isButtonPressed = true
+                    fetchCharacters()
+                    sendPostRequest()
+                    checkIfAlive()
                 } label: {
                     Text("Fight!")
                         .frame(width: 100, height: 30)
                 }
                 .buttonStyle(BorderedButtonStyle())
                 .cornerRadius(40)
+                .disabled(buttonDisabled)
             }
             .navigationTitle("Kick")
-        }
-        .task {
-            do {
-                character1 = try await getCharacter()
-            } catch GHError.invalidURL {
-                print("Invalid URL")
-            } catch GHError.invalidResponse {
-                print("Invalid response")
-            } catch GHError.invalidData {
-                print("Invalid data")
-            } catch {
-                print("Unexpected error")
+            .task {
+                fetchCharacters()
             }
+        }
+        .sheet(isPresented: $showSheet) {
+            Image(winnerImage)
+                .cornerRadius(30)
+            Text("\(winner) wins!")
+                .presentationDetents([.medium])
+                .presentationCornerRadius(45)
+                .presentationBackground(.thinMaterial)
         }
     }
 }
@@ -93,7 +152,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
 
 struct Character: Codable {
     let name: String
@@ -114,8 +172,34 @@ enum GHError: Error {
     case invalidData
 }
 
-func getCharacter() async throws -> Character {
-    let endpoint = "http://127.0.0.1:5000/json_endpoint"
+func sendPostRequest() {
+    let urlString = "http://127.0.0.1:5000/fight_character"
+    guard let url = URL(string: urlString) else {
+        print("Invalid URL")
+        return
+    }
+
+    // Create the POST request
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+
+    // Define the POST data (if needed)
+    let postData = "c1damage=damage".data(using: .utf8)
+
+    // Set the HTTP body
+    request.httpBody = postData
+
+    // Create a URLSession task to send the request
+    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+        if let error = error {
+            print("Error: \(error)")
+        } 
+    }
+    task.resume()
+}
+
+func getCharacter1() async throws -> Character {
+    let endpoint = "http://127.0.0.1:5000/character1"
     
     guard let url = URL(string: endpoint) else { throw GHError.invalidURL }
     
@@ -132,3 +216,23 @@ func getCharacter() async throws -> Character {
         throw GHError.invalidData
     }
 }
+
+func getCharacter2() async throws -> Character {
+    let endpoint = "http://127.0.0.1:5000/character2"
+    
+    guard let url = URL(string: endpoint) else { throw GHError.invalidURL }
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+        throw GHError.invalidResponse
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(Character.self, from: data)
+    } catch {
+        throw GHError.invalidData
+    }
+}
+
